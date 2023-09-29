cd4326ed64c132d6b1881f483f4ba0dcf2247dc8
leewebstechnologies <aliuajibade317@gmail.com>
2023-09-29 22:33:23 +0100

    first commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,9 @@
-export const schemaTypes = []
+export const schemaTypes = [testimonials]
+import testimonials from './testimonials'
+import about from './about'
+import brands from './brands'
+import contact from './contact'
+import experience from './experience'
+import skills from './skills'
+import work from './work'
+import workExperience from './workExperience'
